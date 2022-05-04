517e6325bcdf55a4d18709b0d33cbaf44db12035
Mariella CC <MariellaCC@users.noreply.github.com>
2022-05-05 01:20:29 +0200

    add quick code view mock

--- app.py
+++ app.py
@@ -12,11 +12,36 @@ nb_view = html.Div(
     [
         dbc.Button("Open notebook view", id="open-offcanvas", n_clicks=0),
         dbc.Offcanvas(
-            html.P(
-                "This may contain static notebook view mock-up at later stage."
-            ),
+            html.Div(children=[
+                dbc.Row(dbc.Button("download .ipynb", color="light", id='export-ipynb', size="sm", style={"float":"right", "margin-bottom":"1em"}),),
+                html.H5("1. Corpus information"),
+                dcc.Markdown('''
+                 ```
+
+                publications_list =  [file for file in os.listdir(f'./datasets/{folder}/') if file != '.DS_Store'  ]
+
+                files_list = []
+
+                for pub in publications_list:
+                    files = os.listdir(f"./datasets/{folder}/{pub}/")
+                    files_list.append(files)
+                
+                files_list_flat = [item for sublist in files_list for item in sublist]
+                
+                sources = pd.DataFrame(files_list_flat, columns=['file_name'])")
+                ''', style={"border":".2px solid grey"}),
+        
+                dbc.Textarea(className="mb-3", placeholder="Notes.."),
+
+                html.H5("2. Subset selection"),
+                dcc.Markdown('''
+                 ```
+                sources_subset = sources[sources[date] == 1917]")
+                ''', style={"border":".2px solid grey"}),
+        
+                dbc.Textarea(className="mb-3", placeholder="Notes..")]),
             id="offcanvas",
-            title="Title",
+            title="Topic modelling - ChroniclItaly collection",
             placement="end",
             is_open=False,
             
