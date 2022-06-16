5d68566e41b73d256443604fe166aefd308172e4
Mariella CC <MariellaCC@users.noreply.github.com>
2022-06-16 16:02:26 +0200

    wip run op without saving intermediary res

--- processing_steps/onboarding.py
+++ processing_steps/onboarding.py
@@ -4,40 +4,42 @@ from kiara.interfaces.python_api.operation import KiaraOperation
 
 
 def onboard_df(corpus,alias):
+    print('onboarding')
     
     kiara = Kiara.instance()
-   
-    op = KiaraOperation(kiara=kiara, operation_name="import.file_bundle")
-    inputs = {"path": corpus}
-    job_id = op.queue_job(**inputs)
 
+    import_fb = KiaraOperation(kiara=kiara, operation_name="import.file_bundle")
     try:
-        op.save_result(
-        job_id=job_id, aliases={'file_bundle': 'text_file_bundle'}
-    )
+        import_res = import_fb.run(path=corpus)
+        file_bundle = import_res.get_value_obj('file_bundle')
     except Exception:
+        print('sthg wrong first op')
         pass
 
-
-    op = KiaraOperation(kiara=kiara, operation_name="create.table.from.text_file_bundle")
-    inputs = {"text_file_bundle": 'alias:text_file_bundle'}
-    job_id = op.queue_job(**inputs)
-
+    create_table = KiaraOperation(kiara=kiara, operation_name="create.table.from.text_file_bundle")
+    inputs = {'text_file_bundle': file_bundle}
+    job_id = create_table.queue_job(**inputs)
+    
     try:
-        op.save_result(
+        create_table.save_result()
         job_id=job_id, aliases={"table": alias}
-    )
-    except Exception:
-        pass
 
+    except Exception:
+        print('sthg wrong 2nd op')
+        pass
 
     table_value = kiara.data_registry.get_value(f'alias:{alias}')
 
+    if not table_value:
+        print('Table not found')
+        return ('Table not found')
+    
+    else:
+        print('ok table')
+        table_obj = table_value.data
+        arrow_table = table_obj.arrow_table
+        df = arrow_table.to_pandas()
 
-    actual_table_obj = table_value.data
-    arrow_table = actual_table_obj.arrow_table
-    df = arrow_table.to_pandas()
-
-    return df.head(), df.tail()
+        return df.head(), df.tail()
 
 
